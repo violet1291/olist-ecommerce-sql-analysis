e09b6a9cf52c47fa04ad6839d7cfc850131c1557
violet1291 <alejandradgarcia12@gmail.com>
2026-01-17 22:02:27 -0600

    Dataset

--- README.md
+++ README.md
@@ -10,9 +10,11 @@ This dataset represents orders made between 2016 and 2018, including information
 The data is organized in a relational schema, which makes it ideal for SQL-based analysis. The main tables used are the following: olist_orders_dataset, olist_order_items_dataset, olist_customers_dataset, olist_sellers_dataset, olist_products_dataset, product_category_name_translation (Translations of product category names from Portuguese to English) and olist_order_reviews_dataset.
 
 **Relational Model**
+
 <img width="740" height="340" alt="image" src="https://github.com/user-attachments/assets/2b1a7498-d2e9-4171-bf01-6fbda4899e2c" />
 
 **Dataset’s Limitations and Considerations:**
+
 1.	 Some months and years contain missing data, particularly in the order_approved_at column. A NULL value indicates that, at the time the dataset was released, the order had not yet been approved. These observations represent approximately 0.16% of the dataset and were excluded from the analysis to avoid introducing noise in time-based metrics. 
 2.	The location does not allow a connection between columns, so a geospatial analysis is not done.
 3.	Not all orders have reviews; therefore, analysis involving satisfaction are based only on the subset of reviewed subsets.
@@ -23,6 +25,7 @@ The data is organized in a relational schema, which makes it ideal for SQL-based
 
 
 **Dataset preparation and cleaning**
+
 To start the analysis, a dedicated SQL database (Brazilian_Ecommerce) is created. All CSV files from the Olist dataset are imported as tables using SQL Server’s Import Flat File feature. Once all tables are imported into the dataset, foreign key constraints are added between tables. To see in more detail go to the file “Setting up the dataset”.
 
 Product category data required additional cleaning. Some products contained NULL values in the product category field; therefore, a new category labeled NE (Not Specified) was added to the category translation table to preserve these observations. Also, invalid and redundant rows in the translation table were removed.
@@ -32,11 +35,13 @@ Additionally, missing category translations were identified using a LEFT JOIN co
 
 # Main Results
 **Sales analysis**
+
 According to the analyzed data, total sales in 2016 amounted to USD 49,785.92 (let’s assume the data is in U.S. dollars). This relatively low value is expected, as the dataset only includes transactions starting in September 2016. In 2017, total sales increased significantly to USD 6,145,900.29. Sales continued to grow in 2018, reaching USD 7,394,287.06. Although the 2018 data only covers transactions up to September, total sales for this period still exceed those recorded in 2017.
 
 When comparing cumulative sales from January to September in both years, overall sales increased by 49.75% from 2017 to 2018. This indicates strong year-over-year growth, suggesting that the business was expanding rapidly during this period.
 
 **Categories with the highest revenue**
+
 The top 5 highest products by price value are from the categories: “computer accessories”, “bed bath table”, “garden tools” and lastly, “furniture decor” with prices from USD 54 to USD 138 approximately.
 
 ![alt text](image.png)
--- Setting Up Database.sql
+++ Setting Up Database.sql
@@ -31,54 +31,54 @@ REFERENCES olist_customers_dataset(customer_id);
 ALTER TABLE olist_order_reviews_dataset
 ADD CONSTRAINT FK_order_and_review
 FOREIGN KEY (order_id)
-REFERENCES olist_orders_dataset(order_id)
+REFERENCES olist_orders_dataset(order_id);
 
 --Connection between: olist_orders_dataset and olist_order_payments_dataset
 
 ALTER TABLE olist_order_payments_dataset
 ADD CONSTRAINT FK_payments_orders
 FOREIGN KEY (order_id)
-REFERENCES olist_orders_dataset(order_id)
+REFERENCES olist_orders_dataset(order_id);
 
 --Connection between: olist_order_items_dataset and olist_products_dataset
 
 ALTER TABLE olist_order_items_dataset
 ADD CONSTRAINT FR_product_items_order
 FOREIGN KEY (product_id)
-REFERENCES olist_products_dataset(product_id)
+REFERENCES olist_products_dataset(product_id);
 
 --Connection between: olist_order_items_dataset and olist_sellers_dataset
 
 ALTER TABLE olist_order_items_dataset
 ADD CONSTRAINT FK_order_item_sellers
 FOREIGN KEY (seller_id)
-REFERENCES olist_sellers_dataset (seller_id)
+REFERENCES olist_sellers_dataset (seller_id);
 
 --Connection between: olist_sellers_dataset and olist_geolocation_dataset
 
 ALTER TABLE olist_geolocation_dataset
 ADD CONSTRAINT FK_zipcode_sellets
 FOREIGN KEY (geolocation_zip_code_prefix)
-REFERENCES olist_sellers_dataset(seller_zip_code_prefix)
+REFERENCES olist_sellers_dataset(seller_zip_code_prefix);
 
 
 --Adding Primary Key to table product_category_name_translation
 ALTER TABLE product_category_name_translation
 ADD CONSTRAINT PK_translations
-PRIMARY KEY (column1)
+PRIMARY KEY (column1);
 
 --Updating table to be able to make the connection between the translations and products
 UPDATE olist_products_dataset
 SET product_category_name = 'NE'
-WHERE product_category_name IS NULL
+WHERE product_category_name IS NULL;
 
 --Inserting a Not Specified option in the cathegories for NULL Values
 INSERT INTO product_category_name_translation
-VALUES ('NE', 'Not Specified') 
+VALUES ('NE', 'Not Specified') ;
 
 --Deleting cathegory that does not match
 DELETE FROM product_category_name_translation
-WHERE column1 = 'product_category_name'
+WHERE column1 = 'product_category_name';
 
 --Adding two other rows for categories not found un the translations table
 INSERT INTO product_category_name_translation
@@ -87,7 +87,7 @@ VALUES ('portateis_cozinha_e_preparadores_de_alimentos
 '),
 ('pc_gamer'
 ,' pc_gamer'
-)
+);
 
 --Finding categories that are not in the translation table:
 SELECT DISTINCT p.product_category_name
@@ -100,15 +100,13 @@ WHERE t.column1 IS NULL
 
 --Inserting the last value that did not match
 INSERT INTO product_category_name_translation
-VALUES ('portateis_cozinha_e_preparadores_de_alimentos', 'portable_kitchen_and_food_preparators')
-
+VALUES ('portateis_cozinha_e_preparadores_de_alimentos', 'portable_kitchen_and_food_preparators');
 
 --Finally, adding the connections between the products table and the translations.
 ALTER TABLE olist_products_dataset
 ADD CONSTRAINT FK_translations_columns
 FOREIGN KEY (product_category_name)
-REFERENCES product_category_name_translation(column1)
-
+REFERENCES product_category_name_translation(column1);
 
 ----------------------------------------------------------------------------------------------------------------------------------
 --Database is all set!
